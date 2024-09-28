fa40d2885f5fe11ea25e0ce9553b615d6c915e34
Kirill Stepanishin <kirill.stpn.by@gmail.com>
2024-09-28 13:31:32 -0700

    update LevelManager

--- scripts/levelManager.js
+++ scripts/levelManager.js
@@ -1,3 +1,18 @@
 export class LevelManager {
-    
+
+    loadLevel (filename) {
+        fetch(filename)
+        .then(response => response.json())  // Parse the JSON file
+        .then(levelData => {
+            // Now we have the level data
+            console.log('Loaded level data:', levelData);
+            }
+        )
+        this.levelData = this.levelData;
+    }
+
+    update() {
+        //look for collisions
+        
+    }
 }
